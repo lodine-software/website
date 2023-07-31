6ee46c9250bde60f3f856ae32e79899e7483a2c1
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2023-07-31 23:50:20 +0600

    Add Resources and Documentation columns in footer, fix ExternalLink rel attribute & add prettier configuration

---
+++ components/SocialButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href="to" target="_blank" rel="noreferrer noopener" class="button">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    to: String,
+  },
+}
+</script>
