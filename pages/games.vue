478e429e318cf2ccae85326ae5dce7ecdd8ed321
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2024-03-01 16:17:45 +0500

    Uses ldn/store_repo data now

--- pages/games.vue
+++ pages/games.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="container">
     <h1>Games</h1>
-    <p>No games is currently publicly available. Come back here later!</p>
+    <div v-for="product in JSON.parse(data)" class="card">
+      <span class="title">
+        {{ product.title }} ({{ product.date_of_release }})
+      </span>
+      <span class="description">
+        {{ product.description }}
+      </span>
+      <details>
+        <summary>More...</summary>
+        <span>Developed by {{ product.developer }}</span>
+        <span>Published by {{ product.publisher }}</span>
+      </details>
+      <span
+        ><a :href="product.website" target="_blank" class="btn"
+          >Download</a
+        ></span
+      >
+    </div>
   </div>
 </template>
 
 <script setup>
+const { data } = await useFetch(
+  "https://raw.githubusercontent.com/lodinesoftware/store_repo/main/games.json",
+)
+
 useSeoMeta({
   title: "Games",
   description:
