2a53817575acb21f1fa27cf9e3d984f7418c541b
Nitrrine <nitro@team.lodine.xyz>
2024-03-29 22:46:57 +0500

    Added cat image on error page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="error-page">
+    <NuxtLink to="/">
+      <img :src="'https://http.cat/' + error.statusCode + '.jpg'" alt="" />
+    </NuxtLink>
+    <p>Pro Tip: Click on cat to get out from here.</p>
+  </div>
+</template>
+
+<script setup>
+const error = useError()
+</script>
+
+<style lang="scss" scoped>
+.error-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100dvh;
+  background: #000;
+  color: #fff;
+  text-align: center;
+
+  img {
+    user-select: none;
+    pointer-events: none;
+    max-width: 700px;
+    width: 100%;
+  }
+}
+</style>
