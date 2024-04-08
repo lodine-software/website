179fe5429040f3290cb454c0a7e75f3acf7ae410
Nitrrine <nitro@team.lodine.xyz>
2024-04-08 23:23:34 +0500

    Added new button type: secondary

---
+++ pages/dontship/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Do not ship this page to production</h1>
+    <div class="buttons">
+      <a href="#" class="btn">primary</a>
+      <a href="#" class="btn secondary">secondary</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Do not ship",
+})
+</script>
+
+<style lang="scss" scoped>
+.buttons {
+  display: flex;
+  gap: 0.5em;
+}
+</style>
