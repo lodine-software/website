9f9bad734d48de6b20dfd0a8d05264397074623a
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2024-04-12 21:31:40 +0500

    Update buttons

--- pages/dontship/index.vue
+++ pages/dontship/index.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="container">
     <h1>Do not ship this page to production</h1>
-    <div class="buttons">
-      <a href="#" class="btn">primary</a>
-      <a href="#" class="btn secondary">secondary</a>
+    <div class="components">
+      <div class="buttons">
+        <a href="#" class="btn">primary</a>
+        <a href="#" class="btn secondary">secondary</a>
+      </div>
+      <div class="dropdowns">
+        <select class="dropdown">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </select>
+        <select class="dropdown">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </select>
+        <select class="dropdown">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +31,14 @@ useHead({
 </script>
 
 <style lang="scss" scoped>
-.buttons {
+.components {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+}
+
+.buttons,
+.dropdowns {
   display: flex;
   gap: 0.5em;
 }
