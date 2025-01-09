728a4f273e843c2b9fbe1bd90c521edec33abc47
Nitrrine <nitrrine@team.lodine.xyz>
2025-01-09 22:08:32 +0500

    Update email on contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,13 +5,8 @@
       You are free to contact us via email if you want ask question or need to
       tell something.
     </p>
-    <h4>Developers</h4>
     <div>
-      <a href="mailto:nitro@team.lodine.xyz">nitro@team.lodine.xyz</a>
-    </div>
-    <h4>Community Managers</h4>
-    <div>
-      <a href="mailto:mhought@team.lodine.xyz">mhought@team.lodine.xyz</a>
+      <a href="mailto:nitrrine@team.lodine.xyz">nitrrine@team.lodine.xyz</a>
     </div>
     <p>* We might read your message but not respond if that's not necessary</p>
   </div>
